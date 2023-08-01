981d77477d1aa53465892992ce90633dc2656d52
PRABAKARAN32 <130620467+PRABAKARAN32@users.noreply.github.com>
2023-08-01 07:54:24 +0530

    Initial commit.py
    Code is Updated

--- Addition.py
+++ Addition.py
@@ -1,6 +1,8 @@
 def addition(*num):
-  _num = 0
-  for i in num:
-    _num+=i
-    yield _num
-print(addition(2,3,4,5,6,7,7,6,5))
+    _num = 0
+    for i in num:
+        _num += i
+        yield _num
+result = addition(1,2,3,4,5,67,78)
+for i in result:
+    print(i) #result will be printed
